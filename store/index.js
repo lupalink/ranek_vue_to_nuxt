5b82ffc6d0707e1fc90565adb74996f44e15984c
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2021-01-20 17:20:42 -0300

    refactor(store): index
    * funcionando mas ainda em edição

--- store/index.js
+++ store/index.js
@@ -1,3 +1,9 @@
+// refactor(src): Store
+// add(src): Store
+// rm(src): Store 
+
+import { api } from "@/services.js";
+
 export const getters = {
   isAuthenticated(state) {
     return state.auth.loggedIn
@@ -6,27 +12,179 @@ export const getters = {
   loggedInUser(state) {
     return state.auth.user
   },
-
-  UsuarioProdutos(state) {
-    usuario_produtos: null
-  }
 }
 
-export const  mutations = {
-  UPDATE_USUARIO(state, payload) {
-    state.user = Object.assign(state.user, payload);
+export const state = () => ({
+  // usuario: {
+  //   id: "",
+  //   nome: "Diogo",
+  //   email: "",
+  //   senha: "",
+  //   cep: "",
+  //   rua: "",
+  //   numero: "",
+  //   bairro: "",
+  //   cidade: "",
+  //   estado: ""
+  // },
+  usuario_produtos: null
+})
+export const strict = false
+
+// muda o valor de usuário
+// passando dois arguementos, state para ter acesso aos 
+// atributos e payload que vai mandar as informações que for feito um commit na mutação
+export const mutations = {
+  // CHANGE_USER(state , payload) {
+  //   // console.log(payload.nome)
+  //   state.auth.user.nome = payload.nome
+  //   // state.user.senha = payload.senha
+  // },
+  CHANGE_USER(state, payload) {
+    state.auth.user = Object.assign(state.auth.user, payload);
+  },
+
+  UPDATE_USUARIO: function (state, user) {
+    state.authUser = user
+  },
+
+  SET_POST(state, payload){
+    state.usuario = Object.assign({}, state.usuario, payload)
+  },
+  SET_POSTS(state, payload){
+    state.posts = payload
+  },
+  UPDATE_LOGIN(state, payload) {
+    state.login = payload;
+  },
+  // UPDATE_USUARIO(state, payload) {
+  //   state.auth.user = Object.assign(state.auth.user, payload);
+  // },
+  UPDATE_USUARIO_PRODUTOS(state, payload) {
+    state.usuario_produtos = payload;
+  },
+  ADD_USUARIO_PRODUTOS(state, payload) {
+    state.usuario_produtos.unshit(payload);
   }
 }
-
+// add ação para pegar um usuario
 export const actions = {
+
+  async updatePost({commit}, payload){
+    const {data} = await this.$axios.put(`/usuario`, payload)
+    commit('UPDATE_USUARIO', data)
+  },
+
+
+  setPost({ commit }, payload) {
+    commit('SET_POST', payload)
+  },
   getUsuarioProdutos(context) {
-    // o id é passado pelo context
-    // a ação puxa pelo id registrado no login
-    return api
-    .get(`/produto?usuario_id=${context.state.user.id}`)
-    .then(response => {
-      context.commit("UPDATE_USUARIO_PRODUTOS", response.data);
+      return api
+      .get(`/produto?usuario_id=${context.state.auth.user.id}`)
+      .then(response => {
+        context.commit("UPDATE_USUARIO_PRODUTOS", response.data);
+      });
+  },
+  getUsuario(context) {
+    return api.get(`/usuario`).then(response => {
+      context.commit("UPDATE_USUARIO", response.data);
+      // console.log(response.data);
+      // context.commit("UPDATE_LOGIN", true);
     });
+  },
+
+  // ação para criar usuário
+  criarUsuario(context, payload) {
+    context.commit("UPDATE_USUARIO", { id: payload.email });
+    return api.post("/usuario", payload);
+  },  
+  logarUsuario(context, payload) {
+    return api
+      .login({
+        username: payload.email,
+        password: payload.senha
+      })
+      .then(response => {
+        window.localStorage.token = `Bearer ${response.data.token}`;
+      });
+  },
+  deslogarUsuario(context) {
+    context.commit("UPDATE_USUARIO", {
+      id: "",
+      nome: "",
+      email: "",
+      senha: "",
+      cep: "",
+      rua: "",
+      numero: "",
+      bairro: "",
+      cidade: "",
+      estado: ""
+    });
+    window.localStorage.removeItem("token");
+    context.commit("UPDATE_LOGIN", false);
   }
 }
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Vuex from 'vuex'
+// import userModule from './modules/user'
+
+// const createStore = () => {
+//   return new Vuex.Store({
+//     modules: {
+//       user: userModule
+//     }
+//   })
+// }
+// export default createStore
+
+// export const getters = {
+//   isAuthenticated(state) {
+//     return state.auth.loggedIn
+//   },
+
+//   loggedInUser(state) {
+//     return state.auth.user
+//   },
+// }
+
+

c2bc221732ff777671e9ccefd3e58ed0a3ebc8ec
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2021-01-20 17:14:55 -0300

    add(plugins): main
    * função para converter moeda

---
+++ plugins/main.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+Vue.filter("numeroPreco", valor => {
+  valor = Number(valor);
+  if (!isNaN(valor)) {
+    return valor.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    });
+  } else {
+    return "";
+  }
+});

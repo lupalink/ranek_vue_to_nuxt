1782cd14cb4e5c44fc27685465fc47b5fca3b016
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2021-01-27 13:17:01 -0300

    refactor(pages): UsuarioEditar
    * add modelo padrão funcionando

--- pages/Usuario/editar.vue
+++ pages/Usuario/editar.vue
@@ -1,126 +1,53 @@
 <template>
   <!-- 
-    refactor(view): UsuarioEditar
-    add(view): UsuarioEditar
-    rm(view): UsuarioEditar
+    refactor(pages): UsuarioEditar
+    add(pages): UsuarioEditar
+    rm(pages): UsuarioEditar
   -->
-  <form>
-      
-    <label for="nome">Nome</label>
-    <input id="nome" name="nome" type="text" v-model="auth.user.nome">
-   <label for="email">Email</label>
-    <input id="email" name="email" type="email" v-model="auth.user.email">
-    <label for="senha">Senha</label>
-    <input id="senha" name="senha" type="password" v-model="auth.user.senha">
-    <label for="cep">Cep</label>
-   <input id="cep" name="cep" type="text" v-model="auth.user.cep" @keyup="preencherCep"> 
-    <label for="rua">Logradoro</label>
-    <input id="rua" name="rua" type="text" v-model="auth.user.rua">
-    <label for="numero">Numero</label>
-    <input id="numero" name="numero" type="text" v-model="auth.user.numero">
-    <label for="bairro">Bairro</label>
-    <input id="bairro" name="bairro" type="text" v-model="auth.user.bairro">
-    <label for="cidade">Cidade</label>
-    <input id="cidade" name="cidade" type="text" v-model="auth.user.cidade">
-    <label for="estado">Estado</label>    <input id="estado" name="estado" type="text" v-model="auth.user.estado"> 
-    <button class="btn" @click.prevent="atualizarUsuario">Atualizar Usuário</button>
-
-    <!-- <button class="btn" @click.prevent="pegarDados">Pegar Dados</button> -->
-
-
-
-
-
+  <section>
+    <UsuarioForm>
+      <button class="btn" @click.prevent="atualizarUsuario">Atualizar Usuário</button>
+      <!-- <button class="btn" @click.prevent="teste">teste</button> -->
+    </UsuarioForm>
     <ErroNotificacao :erros="erros"/>
-
-  </form>
+  </section>
 </template>
 
 <script>
-  import axios from 'axios'
-  import {mapState, mapGetters, mapMutations} from 'vuex';
-  import { api } from "@/services.js";
-  export default {
-    data() {     
-      return {
-        erros: [], 
-        nome: ''
-      };
-    },
-    computed: {
-      ...mapState(["auth"])
-    },
-    methods: {
-      ...mapMutations(["CHANGE_USER"]),
-
-      atualizarUsuario() {        
-        this.CHANGE_USER({
-        nome: this.auth.user.nome,
-        email: this.auth.user.email,
-        cep: this.auth.user.cep,
-        numero: this.auth.user.numero,
-        rua: this.auth.user.rua,
-        cidade: this.auth.user.cidade,
-        bairro: this.auth.user.bairro,
-        estado: this.auth.user.estado,   
-      });
-
-
-      this.erros = [];
-      api
-        .put(
-          '/usuario',
-          {
-            "nome": this.auth.user.nome ,
-            "email": this.auth.user.email,
-            "cep": this.auth.user.cep,
-            "numero": this.auth.user.numero,
-            "rua": this.auth.user.rua,
-            "bairro": this.auth.user.bairro,
-            "cidade": this.auth.user.cidade,
-            "estado": this.auth.user.estado,
-            "senha": this.auth.user.senha
-          }
-        )
-        .then(() => {
-          // this.$store.dispatch('getUsuario', {
-          // this.$store.dispatch("getUsuario");
-          this.$router.push({ name: "Usuario" });
-        })
-        .catch(error => {
-          console.log('erro')
-          this.erros.push(error.response.data.message);
-        });
-
-
-
-    },
-
-  getCep(cep) {
-    return axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+import UsuarioForm from "@/components/UsuarioForm.vue";
+import { api } from "@/services.js";
+export default {
+  name: "UsuarioEditar",
+  components: {
+    UsuarioForm
   },
-
-    preencherCep() {
-      const cep = this.auth.user.cep.replace(/\D/g, "");
-      if (cep.length === 8) {
-        this.getCep(cep).then(response => {
-          this.auth.user.rua = response.data.logradouro;
-          this.auth.user.bairro = response.data.bairro;
-          this.auth.user.estado = response.data.uf;
-          this.auth.user.cidade = response.data.localidade;
-        });
-      }
-    },      
-    pegarDados(){
-      console.log(this.auth.user.nome)
-    }
+   data() {
+    return {
+      erros: []
+    };
+  },
+  methods: {
+    atualizarUsuario() {
+      this.erros = [];
+      
+      api.put(
+        '/usuario',
+        this.$store.state.usuario
+      )
+      .then(() => {
+        this.$store.dispatch("getUsuario");
+        this.$router.push({ name: "usuario-produto" });
+      })
+      .catch(error => {
+        this.erros.push(error.response.data.message);
+      });
     },
-    created(){
-      // console.log(user)
-    }
-    
 
-  };
+  },
+  created(){
+    this.$store.dispatch("getUsuario");
+  }
+};
 </script>
 
 <style>

40af382c2897104093ae5eab22d81021ad396513
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2021-03-01 17:47:24 -0300

    refactor(components): SinaproCadastroValor
    * ainda em construção

--- components/SinaproCadastroValor.vue
+++ components/SinaproCadastroValor.vue
@@ -1,92 +1,48 @@
 <template>
-    <div id="app">    
-      <!-- {{ddldata}} -->
-      
-<!--       
-      <div v-if="data">
-        <h2>Titulo</h2>
-        <div class="" v-for="(datas, index) in data" :key="index">
-          <span>Serviço:</span>
-            <ul>
-              <li>
-                {{datas.categoria_sub_pai}}
-              </li>
-            </ul>
-        </div>
-      </div>
-      <PaginaCarregando v-else/>
- -->
+  <!-- 
+    rm(components): SinaproCadastroValor
+    refactor(components): SinaproCadastroValor
+    add(components): SinaproCadastroValor
+  -->
+  <div id="apps" >   
+    {{temp}}
+    <ErroNotificacao :erros="erros"/> 
+    <!-- <h3>{{this.$store.state.sinapro_categoria_titulo.nome}}</h3>      -->
+    <ejs-grid
+    :dataSource='data'
+    ref='grid'
 
+    height='273px' 
 
-
-
-
-
-<!-- style="margin-bottom: 10px;" -->
-
-      <!-- <h2 style="margin-bottom: 2%;">Cadastro de Seviços</h2> -->
-      <!-- <span>Cadastro de Seviços</span> -->
-      <!-- <div id='container' style="width:250px;"> -->
-       
-       <!-- <span style="margin-top: 5%">Titulo do serviço</span> -->
-        <!-- <span style="color: #e3165b;"><b>Título do serviço</b></span> -->
-
-        <!-- <br> 
-        <ejs-dropdownlist id='dropdownlistOne'  :dataSource='ddldata' placeholder='Selecione um serviço' :change="onChange">
-        </ejs-dropdownlist>
-      </div> -->
-        <h3>Serviço: ANÚNCIO PARA REVISTA</h3>
-        <h3>Descrição do serviço: PÁGINA DUPLA</h3>   
-        <ejs-grid ref='grid' :dataSource='data' :allowFiltering='true' height='273px' :filterSettings='filterOptions' :toolbar='toolbar'  :editSettings='editSettings' id='SinaproCadastroValor'>  
-          <!-- :toolbar='toolbar'  :editSettings='editSettings' :actionBegin='actionBegin'>   -->
-            <e-columns>
-               <!-- <e-column field='id' headerText='Resgistro ID' textAlign='Right' width=50  :isPrimaryKey='true'></e-column>
-               <e-column field='categoria_sub_filho_id' headerText='Cabeçalho ID' textAlign='Right' width=50  :isPrimaryKey='true'></e-column> -->
-                <e-column field='tres' headerText='Descrição do valor' textAlign='Right' width=50></e-column>
-                <e-column field='quatro' headerText='Valor' width=120></e-column>
-                <!-- <e-column field='categoria_sub_filho' headerText='Cabeçalho' width=120></e-column>
-                <e-column field='item' headerText='Item id' width=100></e-column>
-                <e-column field='valor_categoria' headerText='Descrição valor' width=100></e-column>
-                <e-column field='valor' headerText='Valor' width=100 :format='formatOptions' ></e-column> -->
-            </e-columns>
-        </ejs-grid>
-        <ErroNotificacao :erros="erros"/>
-      
-    </div>
-
-   
+    :toolbar='toolbar' 
+    :editSettings='editSettings' 
+    :actionBegin='actionBegin'
+    :actionComplete='actionComplete'
+    :toolbarClick='toolbarClick'>  
+      <e-columns> 
+        <e-column field='valor_categoria_id' headerText='Descrição do valor' textAlign='Right' width=50 ></e-column>
+        <e-column field='valores_valor' headerText='Valor' width=120></e-column>
+        <!-- <e-column headerText="" width="150" :commands="commands"></e-column> -->
+      </e-columns>
+    </ejs-grid>
+  </div>
 </template>
 <script>
-import { mapState } from "vuex";
+import {mapState, mapMutations} from 'vuex';
 import { api } from "@/services.js";
 import Vue from "vue";
-import { GridPlugin, Filter, Toolbar, Search, Edit} from "@syncfusion/ej2-vue-grids";
+import { GridPlugin, Filter, Toolbar, Search, Edit, CommandColumn} from "@syncfusion/ej2-vue-grids";
 import { DropDownListPlugin} from "@syncfusion/ej2-vue-dropdowns";
 Vue.use(GridPlugin);
 Vue.use(DropDownListPlugin);
 export default {
   data() {
     return {
-      data: null,
-      data_sinapro_titulo: null,
-      veiculo: '',
-      servicos_id: '',
-      categoria_sub_filho: '',
-      usuario_id: '',
-      empresa_id: '',
-      id: '',
-      nome: '',
-      categoria_sub_filho_id: '',
-      item: '',
-      valores_id: '',
-      valores_valor: '',
-
-      categorias: [{
-        categoria_sub_pai: '',
-      }],
+      data: '',  
       erros: [],
       editSettings: { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Dialog' },
-      toolbar: ['Add', 'Edit', 'Delete', 'Update', 'Cancel'],
+      toolbar: [ 'Add', 'Edit', 'Delete', 'Update', 'Cancel', {text: 'Finalziar cadastro', tooltipText: 'Finalziar cadastro', id: 'toolbarfilter', align:'Right'}],
+      
       // toolbarOptions: ['Search'], 
       formatOptions: { format:'C2', currency:'BRL' },
        filterOptions: {
@@ -94,38 +50,66 @@ export default {
         mode: 'Immediate',
         immediateModeDelay: 200,      
       }, 
-      
-      ddldata: null
-      // ddldata: ['TODOS', 'ANÚNCIO PARA REVISTA', 'ANÚNCIO PARA JORNAL']
+      commands: [{ buttonOption: { content: "Selecionar", cssClass: "e-flat" } }],
+      ddldata: null,
+      temp: ''
     };
   },
   provide: {
-    grid: [Filter, Toolbar, Search, Edit]
+    grid: [Toolbar, Search, Edit, CommandColumn]
   },
   computed: {
-    ...mapState(["auth", "login"])
+    ...mapState(['auth', 'login', 'sinapro_categoria_item', 'sinapro_categoria_titulo', 'valores'])   
   },
   methods: {
+    ...mapMutations(["UPDATE_SINAPRO_CATEGORIA_ITEM", "UPDATE_SINAPRO_VALOR"]),
+    toolbarClick: function (args) {
+      if (args.item.id === 'toolbarfilter') {
+        // this.$refs.treegrid.filterByColumn('taskName', 'startswith', 'Testing');
+        // console.log(this.$refs.grid.ej2Instances.currentViewData)
+      }
+    },
+    salvarRegistro(){
+      // console.log(this.$refs.grid.ej2Instances.currentViewData); 
+      let lista = [];
+      this.$refs.grid.ej2Instances.currentViewData.forEach((item) => {     
+        // console.log(item);
+          // listaCadastroTitulo.push(item);
+          lista.push(item);
+          
+        })    
+        this.temp = lista
+        this.UPDATE_SINAPRO_VALOR(this.temp)
+        // this.data = listaCadastroTitulo;
+
+    },
+
+
     getServicos() {
-      api.get(`servicos/buscar?veiculo=sinapro&filtro=servico&filtro=servico&usuario_id=17&servicos_praca_id=7&servicos_insercao_id=7&sinapro_id=1&empresa_id=16`).then(response => {
-        // console.log(response.data.sinapro_categoria_titulo)
-        this.data = response.data.sinapro_categoria_titulo;
-       console.log( response.data.sinapro_categoria_titulo)
+      api.get(`servicos/buscar?veiculo=sinapro&filtro=servico&usuario_id=17&servicos_praca_id=7&servicos_insercao_id=7&sinapro_id=1&empresa_id=16&nivel=3`).then(response => {
         let lista = [];
-        lista.push('TODOS');
-        response.data.sinapro_categoria_titulo.forEach((item) => {     
-          lista.push(item.nome);
-        })
+        let listaCadastroTitulo = [];
+     
+        lista.push('TODOS');   
+        response.data.sinapro_categoria.forEach((item) => {     
         
-        // console.log(lista);
-        // console.log(this.r) 
+          listaCadastroTitulo.push(item);
+          lista.push(item.nome);
+          
+        })    
         this.ddldata = lista
-      });
+        this.data = listaCadastroTitulo;
+ 
+      })
+      .catch(error => {
+        this.erros.push(error.response.data.message);
+        // console.log(error)
+      })
     },
     onChange: function(args) {
       let element = document.createElement('p');
       if (args.isInteracted) {  
-        const filtro =  this.filtro = args.itemData.value
+        // const filtro =  this.filtro = args.itemData.value
         // console.log(this.filtro)        
         if (args.itemData.value == 'TODOS') {
           this.$refs.grid.clearFiltering();
@@ -134,54 +118,107 @@ export default {
         }      
       } 
     },
-    // recordDoubleClick: function(args) { 
-    //  // you can show Dialog component here 
-    //   alert("dblclick"); 
-    //     console.log(args.rowData.sinapro_id); 
-    //  },
-     
+    actionComplete: function (e) { 
+      if (e.requestType === 'save') { 
+        this.UPDATE_SINAPRO_VALOR(this.$refs.grid.ej2Instances.currentViewData)
+        //  console.log(this.$refs.grid.ej2Instances.currentViewData)
+      // here you can get all the data after updating 
+      //  console.log(e); 
+      //  console.log(this.$refs.grid.ej2Instances); 
+      }else if(e.requestType === 'delete') {
+       this.UPDATE_SINAPRO_VALOR(this.$refs.grid.ej2Instances.currentViewData)
+      }
+    }, 
     actionBegin: function(args) { 
-    if (args.requestType === "save") { 
-      // console.log(args.data); // edited data 
-      console.log(args.data); // edited data 
-      // PLease store this data in Vuex store 
-      this.erros = [];
-  
-      this.categoria_sub_filho = args.categoria_sub_filho
-      console.log( args.data.categoria_sub_filho)
-      api.put(`servicos/atualizar/`,{
-        veiculo: 'sinapro',
-        filtro: 'servico',
-        servicos_id: 111,      
-        usuario_id: 17,
-        empresa_id: 16,
-        id: 102,
-        nome: args.data.categoria_sub_pai,
-        categoria_sub_filho: args.data.categoria_sub_filho,
-        categoria_sub_filho_id: 103,
-        item: args.data.item,
-        valores_id: args.data.valor_id,
-        valores_valor: args.data.valor,
-      })
-      .then(() => {
-        this.getServicos();
-        // this.$store.dispatch("getUsuario");
-        // this.$router.push({ name: "usuario-produto" });
-      })
-      .catch(error => {
-        this.erros.push(error.response.data.message);
-        // console.log(error)
-      });
-  
+      
+    if (args.requestType === "save") {      
+      this.erros = [];    
+      
+      // console.log(this.$refs.grid.ej2Instances); 
+      // this.UPDATE_SINAPRO_VALOR({ 
+      //   valor_categoria_id: args.data.valor_categoria_id,
+      //   valores_valor: args.data.valores_valor
+      // })
+      // console.log(args)
+      
+        // this.UPDATE_SINAPRO_VALOR(args.data)
+    
+
+
+
+
+
+
+      // api.post(`/servicos`,
+      // { 
+      //   veiculo: "sinapro",
+	    //   filtro: "titulo",
+      //   usuario_id: 17,
+      //   servicos_insercao_id: 7,
+      //   servicos_insercao_controle_id: 54,
+
+      //   nivel: 3,
+      //   sinapro_id: this.sinapro_categoria_titulo.sinapro_id,      
+      //   nome: args.data.nome,
+      //   sinapro_numero_item: args.data.sinapro_numero_item,
+      //   nivel_descricao: 'SINAPRO',
+      //   sinapro_item_cabecalho_id: 1,  
+      //   sinapro: true,        
+      // })
+      // .then((r) => {
+      //   this.getServicos();                               
+      //   // this.$refs.grid.filterByColumn('nome', 'equal', this.sinapro_categoria_item[0].nome)
+      //   this.$refs.grid.filterByColumn('nome', 'equal', args.data.nome) 
+      // })
+      // .catch(error => {
+      //   this.erros.push(error.response.data.message);
+      //   // console.log(error)
+      // }); 
     } else if (args.requestType === "delete") { 
+        
+      
+      // console.log(args)
        // console.log(args.data.servico_id)
-        console.log(args.data[0]['servico_id'])
+        // console.log(args.data[0]['servico_id'])
        // // alert(args.data.servico_id);      
-      } 
+        
+      }
+    },
+
+    commandClick: function(args) {
+      // debugger;
+      // console.log(args.rowData)
+      var rowID = args.target.parentElement
+      .closest("tr")
+      .getAttribute("data-uid");
+      var rowData = JSON.stringify(args.rowData);
+      // var rowData = args.rowData.nome;
+      var rowIndex = args.target.parentElement
+      .closest("tr")
+      .getAttribute("aria-rowindex");
+      alert(
+        // "rowID: " +
+        //   rowID +
+        //   "\r\nrowIndex: " +
+        //   rowIndex +
+          "\r\nDESCRIÇÃO DO ITEM: " +
+          rowData
+      );
+      this.UPDATE_SINAPRO_CATEGORIA_ITEM({ 
+        id: args.rowData.id,
+        nivel: 2,
+        sinapro_id: this.sinapro_categoria_titulo.sinapro_id, 
+        nome: args.rowData.nome,
+        sinapro: 1,
+        sinapro_numero_item: args.rowData.sinapro_numero_item,
+        sinapro_item_cabecalho_id: 1  
+      })  
     }
   },
   created() {
+    // console.log(this.$store.state.sinapro_categoria_item)
     // this.getServicos();
+    // console.log(this.sinapro_categoria_titulo)
   },
 }
 </script>

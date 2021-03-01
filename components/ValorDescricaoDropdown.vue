386d25ca0f0b05b07e94f13bb4f8a8c18d5cb902
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2021-03-01 17:41:47 -0300

    refactor(components):  ValorDescricaoDropdown
    * para lista a descrição dos valores e retorna dados para eventualmente serem usados no crud

---
+++ components/ValorDescricaoDropdown.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <ejs-dropdownlist
+    name="descricaoValoresDropdown"
+    id="dropdownlist"
+    v-model="data.valor_servico[0].valor_categorias[0].valor_descricao"
+    :change="doCustomAction.bind(this)"
+    placeholder="Selecione um item"
+    :dataSource="valores"
+    :fields="fieldsUsers"
+  ></ejs-dropdownlist>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      valores: this.$store.state.valores,
+      fieldsUsers: { text: "nome", value: "nome" }
+    };
+  },
+  methods: {
+    doCustomAction: function(args) {
+      // console.log(args);
+      console.log(args.value);
+      this.$eventHub.$emit("descricaoDoValor", args.value);
+    },
+  },
+  };
+</script>
+
+<style>
+</style>

740818787e7d69aed1891068e5986bf3ceebd819
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2021-03-04 11:26:51 -0300

    refactor(pages): editar
    * está em fase de testes

---
+++ pages/tabela/_id/detalhes/editar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>{{this.$route.params.id}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  created(){
+    console.log(this.$route.params.id)
+  }
+}
+</script>
+
+<style>
+
+</style>
